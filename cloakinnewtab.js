99d28c12616f8dc2506632be7cc2d323a46ffdbc
sigmaangelo <angelo.frank.george@gmail.com>
2026-02-14 16:58:48 -0500

    Refactor iframe handling in cloakinnewtab.js
    Refactor iframe creation and styling in cloakinnewtab.js.

--- cloakinnewtab.js
+++ cloakinnewtab.js
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Home - PowerUp</title>
+
+    <!-- Favicon -->
     <link rel="icon" href="lexialogo.png" type="image/png">
+
     <style>
         html, body {
             margin: 0;
@@ -11,23 +14,33 @@
             width: 100%;
             height: 100%;
             overflow: hidden;
-        }
-
-        iframe {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100vw;
-            height: 100vh;
-            border: none;
-            z-index: 2147483647;
             background: white;
         }
     </style>
 </head>
 <body>
 
-<iframe src="main.html"></iframe>
+<script>
+(function () {
+    var url = "/main.html";
+
+    // Create iframe
+    var iframe = document.createElement('iframe');
+
+    // Same styles you had before
+    iframe.style = "position:fixed;width:100vw;height:100vh;top:0;left:0;right:0;bottom:0;z-index:2147483647;background-color:white;border:none;";
+
+    // Assign URL safely
+    if (url.includes('https://') || url.includes('http://')) {
+        iframe.src = url;
+    } else {
+        iframe.src = "https://" + url;
+    }
+
+    // Add iframe to current page
+    document.body.appendChild(iframe);
+})();
+</script>
 
 </body>
 </html>
